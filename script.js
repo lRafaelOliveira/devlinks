9ae8fb23f8759fb21d283b55299618cad047caf3
Rafael Oliveira <63177618+lRafaelOliveira@users.noreply.github.com>
2024-01-21 18:23:14 -0300

    Update

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar.png")
   }
 }
+
+function ActiveGerarQrCode() {
+  document.querySelector('#lista-links').style.display = 'none';
+  document.querySelector('#gerarqrcode').style.display = 'block';
+}
+
+function voltarGerarQrCode() {
+  document.querySelector('#lista-links').style.display = 'flex';
+  document.querySelector('#gerarqrcode').style.display = 'none';
+
+}
+
+function gerarQrCode() {
+
+  let texto = document.querySelector('#link-qrcode').value
+  var qr = qrcode(4, 'M');
+  qr.addData(texto);
+  qr.make();
+
+  // Obtém o elemento HTML onde o QR code será exibido
+  var elementoQRCode = document.getElementById('qrcode');
+
+  // Gera e exibe o QR code no elemento
+  elementoQRCode.innerHTML = qr.createImgTag();
+}
